94f04390e69fec7a1bb87b75e3b4542682b60fba
Laura DeCicco <ldecicco@usgs.gov>
2014-07-03 09:55:20 -0500

    Initial functions getDataAvailability, whatUV, and importRDB. I think we should choose getDataAvailablity over whatUV.

---
+++ R/USGSwsDataRetrieval.R
@@ -0,0 +1,33 @@
+#' Retrieval functions for USGS data
+#'
+#' \tabular{ll}{
+#' Package: \tab USGSwsDataRetrieval\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.0.0\cr
+#' Date: \tab 2014-07-03\cr
+#' License: \tab Unlimited for this package, dependencies have more restrictive licensing.\cr
+#' Copyright: \tab This software is in the public domain because it contains materials
+#' that originally came from the United States Geological Survey, an agency of
+#' the United States Department of Interior. For more information, see the
+#' official USGS copyright policy at
+#' http://www.usgs.gov/visual-id/credit_usgs.html#copyright\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' Collection of functions to help retrieve USGS data from either web services or user provided data files.
+#'
+#' @name USGSwsDataRetrieval-package
+#' @import RCurl XML reshape2
+#' @docType package
+#' @author David Lorenz \email{lorenz@@usgs.gov}, Laura De Cicco \email{ldecicco@@usgs.gov}
+#' @keywords data, retrieval
+NULL
+
+#' List of USGS parameter codes
+#'
+#' Complete list of USGS parameter codes as of September 25, 2013.
+#'
+#' @name parameterCdFile
+#' @docType data
+#' @keywords USGS parameterCd
+NULL
